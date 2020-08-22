daf1de5f02a4bf2e1c0d36c2ea860c40453e1342
jatinsajwan3841 <58560850+jatinsajwan3841@users.noreply.github.com>
2020-08-22 17:49:52 +0530

    created main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from sup import result
+
+re = '1'
+while re == '1':
+    s = result()
+    s.select(1)
+    s.select(2)
+    s.select(3)
+    s.Display()
+
+    re = input("sab sahi he na ya dubara chalana chahte ho ise? \t To run again enter 1 else press any key")
+    s.clear_screen()
+
+else:
+    exit()
